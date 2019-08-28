810bb7f02a71d3ee38f42d49e7a5eba66ad74b97
fyang1024 <fyang1024@gmail.com>
2019-08-28 14:20:14 +1000

    Clean up Dockerfiles

--- reference-test/support/docker-init.sh
+++ reference-test/support/docker-init.sh
@@ -3,39 +3,19 @@
 for i in "$@"
 do
 	case $i in
-		--consulmaster=*)
-		    CONSUL_MASTER=`echo $i | sed 's/[-a-zA-Z0-9\.]*=//'`
-		;;
-                --consul.enabled=*)
-                    CONSUL_ENABLED=`echo $i | sed 's/[-a-zA-Z0-9\.]*=//'`	    
-                ;;
-                --baseUri=*)
-                    TARGET_URI=`echo $i | sed 's/[-a-zA-Z0-9\.]*=//'`
-                ;;
-                -b=*)
-                    TARGET_URI=`echo $i | sed 's/[-a-zA-Z0-9\.]*=//'`
-                ;;
-                -s)
-                    SERVE_CONTENT=true
-                ;;
+    --baseUri=*)
+    TARGET_URI=`echo $i | sed 's/[-a-zA-Z0-9\.]*=//'`
+    ;;
+    -b=*)
+    TARGET_URI=`echo $i | sed 's/[-a-zA-Z0-9\.]*=//'`
+    ;;
+    -s)
+    SERVE_CONTENT=true
+    ;;
 		*)
 		;;
 	esac
 done
-
-if [ "$CONSUL_ENABLED" == true ]
-    then
-        # Configure dns
-        cp /etc/resolv.conf /etc/resolv.conf.dnsmasq
-        echo "search service.dc1.consul." > /etc/resolv.conf
-        echo "nameserver 127.0.0.1" >> /etc/resolv.conf
-        /usr/sbin/dnsmasq --server=/consul/127.0.0.1#8600 -r /etc/resolv.conf.dnsmasq -u root
-        mkdir -p /opt/consul
-        /usr/bin/consul agent -join=$CONSUL_MASTER -data-dir=/opt/consul &
-        sleep 5
-        /usr/bin/consul info
-    fi
-
 
 cd /opt/cds-java-artefacts/reference-test
 
