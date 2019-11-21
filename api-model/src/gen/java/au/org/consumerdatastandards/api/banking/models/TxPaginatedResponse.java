692a651669d41fa5b47d6d0ec1d430d9de69b9a9
fyang1024 <fyang1024@gmail.com>
2019-11-21 11:42:22 +1100

    Fix #51 - add TxMetaPaginated to api-model

--- api-model/src/gen/java/au/org/consumerdatastandards/api/banking/models/TxPaginatedResponse.java
+++ api-model/src/gen/java/au/org/consumerdatastandards/api/banking/models/TxPaginatedResponse.java
@@ -1,8 +1,10 @@
 package au.org.consumerdatastandards.api.banking.models;
 
 import au.org.consumerdatastandards.api.common.models.LinksPaginated;
+import au.org.consumerdatastandards.support.data.DataDefinition;
 import au.org.consumerdatastandards.support.data.Property;
 
+@DataDefinition
 public class TxPaginatedResponse {
 
     @Property(
