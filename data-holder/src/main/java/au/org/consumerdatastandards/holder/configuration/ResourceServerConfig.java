bad885249ee0f755d4d68dd29589489d27793891
fyang1024 <fyang1024@gmail.com>
2019-12-11 16:43:34 +1100

    #53 configure account api scopes

--- data-holder/src/main/java/au/org/consumerdatastandards/holder/configuration/ResourceServerConfig.java
+++ data-holder/src/main/java/au/org/consumerdatastandards/holder/configuration/ResourceServerConfig.java
@@ -13,9 +13,13 @@ public class ResourceServerConfig extends WebSecurityConfigurerAdapter {
         http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.IF_REQUIRED)
             .and().authorizeRequests()
             .antMatchers("/**/banking/products/**").permitAll()
-            .antMatchers("/**/banking/accounts/**").access("hasAuthority('SCOPE_accounts.read')")
+            .antMatchers("/**/banking/accounts").access("hasAuthority('SCOPE_bank:accounts.basic:read')")
+            .antMatchers("/**/banking/accounts/balances").access("hasAuthority('SCOPE_bank:accounts.basic:read')")
+            .antMatchers("/**/banking/accounts/**/balance").access("hasAuthority('SCOPE_bank:accounts.basic:read')")
+            .antMatchers("/**/banking/accounts/**").access("hasAuthority('SCOPE_bank:accounts.detail:read')")
+            .antMatchers("/**/banking/accounts/**/transactions/**").access("hasAuthority('SCOPE_bank:transactions:read')")
             .antMatchers("/**/banking/payees/**").access("hasAuthority('SCOPE_payees.read')")
-            .antMatchers("/**/banking/**/direct-debits/**").access("hasAuthority('SCOPE_direct-debits,read')")
+            .antMatchers("/**/banking/**/direct-debits/**").access("hasAuthority('SCOPE_direct-debits.read')")
             .antMatchers("/**/common/customer/**").access("hasAuthority('SCOPE_customer.read')")
             .and().oauth2ResourceServer().jwt();
     }
