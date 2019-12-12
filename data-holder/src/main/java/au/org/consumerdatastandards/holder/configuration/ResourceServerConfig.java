0cb84ba73dfc0adb716e58f50b64d1840eadb6ae
fyang1024 <fyang1024@gmail.com>
2019-12-12 16:11:52 +1100

    #53 configure api scopes

--- data-holder/src/main/java/au/org/consumerdatastandards/holder/configuration/ResourceServerConfig.java
+++ data-holder/src/main/java/au/org/consumerdatastandards/holder/configuration/ResourceServerConfig.java
@@ -1,12 +1,18 @@
 package au.org.consumerdatastandards.holder.configuration;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.oauth2.jwt.JwtDecoder;
+import org.springframework.security.oauth2.jwt.NimbusJwtDecoder;
 
 @EnableWebSecurity
 public class ResourceServerConfig extends WebSecurityConfigurerAdapter {
+
+    @Value("${spring.security.oauth2.resourceserver.jwt.jwk-set-uri}") String jwkSetUri;
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
@@ -18,9 +24,15 @@ public class ResourceServerConfig extends WebSecurityConfigurerAdapter {
             .antMatchers("/**/banking/accounts/**/balance").access("hasAuthority('SCOPE_bank:accounts.basic:read')")
             .antMatchers("/**/banking/accounts/**").access("hasAuthority('SCOPE_bank:accounts.detail:read')")
             .antMatchers("/**/banking/accounts/**/transactions/**").access("hasAuthority('SCOPE_bank:transactions:read')")
-            .antMatchers("/**/banking/payees/**").access("hasAuthority('SCOPE_payees.read')")
-            .antMatchers("/**/banking/**/direct-debits/**").access("hasAuthority('SCOPE_direct-debits.read')")
-            .antMatchers("/**/common/customer/**").access("hasAuthority('SCOPE_customer.read')")
+            .antMatchers("/**/banking/payees/**").access("hasAuthority('SCOPE_bank:payees:read')")
+            .antMatchers("/**/banking/**/direct-debits/**").access("hasAuthority('SCOPE_bank:regular_payments:read')")
+            .antMatchers("/**/common/customer").access("hasAuthority('SCOPE_common:customer.basic:read')")
+            .antMatchers("/**/common/customer/detail").access("hasAuthority('SCOPE_common:customer.detail:read')")
             .and().oauth2ResourceServer().jwt();
+    }
+
+    @Bean
+    JwtDecoder jwtDecoder() {
+        return NimbusJwtDecoder.withJwkSetUri(this.jwkSetUri).build();
     }
 }
