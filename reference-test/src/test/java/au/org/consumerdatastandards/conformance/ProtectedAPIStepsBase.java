a08a5b2c64fae3d5ba63b7bec2728bd251525c78
Vadym <30234255+vadkor@users.noreply.github.com>
2019-12-18 16:13:14 +1100

    #48: Working on quering config, introspection and token endpoints.

--- reference-test/src/test/java/au/org/consumerdatastandards/conformance/ProtectedAPIStepsBase.java
+++ reference-test/src/test/java/au/org/consumerdatastandards/conformance/ProtectedAPIStepsBase.java
@@ -1,15 +1,62 @@
 package au.org.consumerdatastandards.conformance;
 
 import au.org.consumerdatastandards.support.Header;
+import io.restassured.http.ContentType;
+import io.restassured.response.ExtractableResponse;
+import io.restassured.response.Response;
 import io.restassured.specification.RequestSpecification;
 
+import java.io.IOException;
 import java.time.OffsetDateTime;
+import java.util.HashMap;
+import java.util.Map;
+
+import static net.serenitybdd.rest.SerenityRest.given;
 
 public class ProtectedAPIStepsBase extends APIStepsBase {
 
+    public static final String INTROSPECTION_ENDPOINT = "introspection_endpoint";
+    public static final String TOKEN_ENDPOINT = "token_endpoint";
+
+    private static final HashMap<String, Map<String, String>> discoveredConfigs = new HashMap<>();
+
     protected RequestSpecification buildHeaders(RequestSpecification given) {
+        String accessToken = props.getProperty("access.token");
+        try {
+            String authServer = props.getProperty("auth.server");
+            Map<String, String> config = discoveredInfo(authServer);
+            boolean valid = given()
+                    .contentType("application/x-www-form-urlencoded")
+                    .body("token=" + accessToken).when().post(authServer + config.get(INTROSPECTION_ENDPOINT))
+                    .then().log().all().contentType(ContentType.JSON).extract().path("active");
+            if (!valid) {
+                accessToken = acquireNewAccessToken(config.get(TOKEN_ENDPOINT));
+            }
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
         return super.buildHeaders(given)
-                .header("Authorization", "Bearer " + props.getProperty("access.token"))
+                .header("Authorization", "Bearer " + accessToken)
                 .header(Header.FAPI_AUTH_DATE.getKey(), OffsetDateTime.now().toString());
+    }
+
+    private String acquireNewAccessToken(String tokenEndpoint) {
+        String refreshToken = props.getProperty("access.token");
+
+        return null; // TODO implement me
+    }
+
+    private static Map<String, String> discoveredInfo(String authServer) throws IOException {
+        Map<String, String> config = discoveredConfigs.get(authServer);
+        if (config == null) {
+            ExtractableResponse<Response> resp = given().when().get(authServer + "/.well-known/openid-configuration")
+                    .then().log().all().contentType(ContentType.JSON).extract();
+            config = new HashMap<>();
+            config.put(TOKEN_ENDPOINT, resp.path(TOKEN_ENDPOINT));
+            config.put(INTROSPECTION_ENDPOINT, resp.path(INTROSPECTION_ENDPOINT));
+            discoveredConfigs.put(authServer, config);
+        }
+        return config;
     }
 }
