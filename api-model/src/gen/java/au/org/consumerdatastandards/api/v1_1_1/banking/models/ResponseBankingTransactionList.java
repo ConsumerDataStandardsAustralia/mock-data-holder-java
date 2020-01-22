25fba287413501f9c3a32577c354891e1b248d0a
fyang1024 <fyang1024@gmail.com>
2020-01-22 14:32:32 +1100

    Add missing models

--- api-model/src/gen/java/au/org/consumerdatastandards/api/v1_1_1/banking/models/ResponseBankingTransactionList.java
+++ api-model/src/gen/java/au/org/consumerdatastandards/api/v1_1_1/banking/models/ResponseBankingTransactionList.java
@@ -1,11 +1,10 @@
 package au.org.consumerdatastandards.api.v1_1_1.banking.models;
 
-import au.org.consumerdatastandards.api.v1_1_1.common.models.PaginatedResponse;
 import au.org.consumerdatastandards.support.data.DataDefinition;
 import au.org.consumerdatastandards.support.data.Property;
 
 @DataDefinition
-public class ResponseBankingTransactionList extends PaginatedResponse {
+public class ResponseBankingTransactionList extends TxPaginatedResponse {
 
     @Property(
         required = true
