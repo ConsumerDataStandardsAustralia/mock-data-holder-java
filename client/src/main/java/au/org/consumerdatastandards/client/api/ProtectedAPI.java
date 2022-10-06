6f61f48f1cc99f6006c06812692170714e4fc4fb
abeheron <abeheron@gmail.com>
2022-10-07 09:24:55 +1100

    Updated client header 'x-fapi-auth-date' to follow RFC7231 (HTTP-DATE) format and be FAPI compliant.

--- client/src/main/java/au/org/consumerdatastandards/client/api/ProtectedAPI.java
+++ client/src/main/java/au/org/consumerdatastandards/client/api/ProtectedAPI.java
@@ -4,14 +4,16 @@ import java.net.DatagramSocket;
 import java.net.InetAddress;
 import java.net.SocketException;
 import java.net.UnknownHostException;
-import java.time.OffsetDateTime;
+import java.time.ZoneOffset;
+import java.time.ZonedDateTime;
+import java.time.format.DateTimeFormatter;
 import java.util.Map;
 
 public class ProtectedAPI extends UnprotectedAPI {
 
     protected void addCdsProtectedApiHeaders(Map<String, String> headers) {
         headers.put("x-fapi-customer-ip-address", getIpAddress());
-        headers.put("x-fapi-auth-date", OffsetDateTime.now().toString());
+        headers.put("x-fapi-auth-date", DateTimeFormatter.RFC_1123_DATE_TIME.format(ZonedDateTime.now(ZoneOffset.UTC)));
     }
 
     private String getIpAddress() {
