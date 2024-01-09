7c1c1b46e3bdd4039ffd1405d8e728f2d08c94b0
Vadym <30234255+vadkor@users.noreply.github.com>
2024-01-09 15:14:18 +1100

    Updated Common to CDS spec 1.29.0

--- client/src/main/java/au/org/consumerdatastandards/client/api/CommonCustomerAPI.java
+++ client/src/main/java/au/org/consumerdatastandards/client/api/CommonCustomerAPI.java
@@ -1,8 +1,6 @@
 /*
  * Consumer Data Standards
  * Sample client library to Demonstrate the Consumer Data Right APIs
- *
- * NOTE: This class is auto generated
  */
 package au.org.consumerdatastandards.client.api;
 
@@ -72,7 +70,7 @@ public class CommonCustomerAPI extends ProtectedAPI {
 
     /**
      * Get Customer
-     * Obtain basic information on the customer that has authorised the current session
+     * Obtain basic information on the customer that has authorised the current session\n\n<h3 id='cdr-common-api_get-customer_conventions'>Conventions</h3>\nIn the customer payloads relevant conventions are explained here, in one place.\n\n#### Given Names\n\n`firstName` represents the first of a person's given names.\n\n`middleNames` represents a collection of given names if the person has more than one given name.\n\nWhere a data holder holds a person's given names as a single string in source systems, it may not possible in some situations to reliably split these given names into their component first and middle names. In these situations, data holders MAY use the `firstName` field to return the single string of given names and an empty `middleNames` array.\n\nFor example, a person whose given names are \"John Paul Winston\" but the data holder cannot determine what is the first name, can return `\"firstName\": \"John Paul Winston\"`.
      * @return ResponseCommonCustomer
      * @throws ApiException If fail to call the API, e.g. server error or cannot deserialize the response body
      * http.response.details
@@ -88,8 +86,7 @@ public class CommonCustomerAPI extends ProtectedAPI {
      *        <td>  * x-v - The [version](#response-headers) of the API end point that the data holder has responded with. <br>  </td>
      *    </tr>
      * </table>
-
-*/
+     */
     public ResponseCommonCustomer getCustomer() throws ApiException {
 
         LOGGER.trace("getCustomer");
@@ -100,7 +97,7 @@ public class CommonCustomerAPI extends ProtectedAPI {
 
     /**
      * Get Customer
-     * Obtain basic information on the customer that has authorised the current session
+     * Obtain basic information on the customer that has authorised the current session\n\n<h3 id='cdr-common-api_get-customer_conventions'>Conventions</h3>\nIn the customer payloads relevant conventions are explained here, in one place.\n\n#### Given Names\n\n`firstName` represents the first of a person's given names.\n\n`middleNames` represents a collection of given names if the person has more than one given name.\n\nWhere a data holder holds a person's given names as a single string in source systems, it may not possible in some situations to reliably split these given names into their component first and middle names. In these situations, data holders MAY use the `firstName` field to return the single string of given names and an empty `middleNames` array.\n\nFor example, a person whose given names are \"John Paul Winston\" but the data holder cannot determine what is the first name, can return `\"firstName\": \"John Paul Winston\"`.
      * @return ApiResponse&lt;ResponseCommonCustomer&gt;
      * @throws ApiException If fail to call the API, e.g. server error or cannot deserialize the response body
      * http.response.details
@@ -116,8 +113,7 @@ public class CommonCustomerAPI extends ProtectedAPI {
      *        <td>  * x-v - The [version](#response-headers) of the API end point that the data holder has responded with. <br>  </td>
      *    </tr>
      * </table>
-
-*/
+     */
     public ApiResponse<ResponseCommonCustomer> getCustomerWithHttpInfo() throws ApiException {
         okhttp3.Call call = getCustomerValidateBeforeCall(null);
         Type returnType = new TypeToken<ResponseCommonCustomer>(){}.getType();
@@ -126,7 +122,7 @@ public class CommonCustomerAPI extends ProtectedAPI {
 
     /**
      * Get Customer (asynchronously)
-     * Obtain basic information on the customer that has authorised the current session
+     * Obtain basic information on the customer that has authorised the current session\n\n<h3 id='cdr-common-api_get-customer_conventions'>Conventions</h3>\nIn the customer payloads relevant conventions are explained here, in one place.\n\n#### Given Names\n\n`firstName` represents the first of a person's given names.\n\n`middleNames` represents a collection of given names if the person has more than one given name.\n\nWhere a data holder holds a person's given names as a single string in source systems, it may not possible in some situations to reliably split these given names into their component first and middle names. In these situations, data holders MAY use the `firstName` field to return the single string of given names and an empty `middleNames` array.\n\nFor example, a person whose given names are \"John Paul Winston\" but the data holder cannot determine what is the first name, can return `\"firstName\": \"John Paul Winston\"`.
      * @param _callback The callback to be executed when the API call finishes
      * @return The request call
      * @throws ApiException If fail to process the API call, e.g. serializing the request body object
@@ -143,9 +139,7 @@ public class CommonCustomerAPI extends ProtectedAPI {
      *        <td>  * x-v - The [version](#response-headers) of the API end point that the data holder has responded with. <br>  </td>
      *    </tr>
      * </table>
-
-
-*/
+     */
     public okhttp3.Call getCustomerAsync(final ApiCallback<ResponseCommonCustomer> _callback) throws ApiException {
 
         LOGGER.trace("Asynchronously getCustomer");
@@ -173,7 +167,7 @@ public class CommonCustomerAPI extends ProtectedAPI {
      *        <td>  * x-v - The [version](#response-headers) of the API end point that the data holder has responded with. <br>  * x-fapi-interaction-id - An [RFC4122](https://tools.ietf.org/html/rfc4122) UUID used as a correlation id. If provided, the data holder must play back this value in the x-fapi-interaction-id response header. If not provided a [RFC4122] UUID value is required to be provided in the response header to track the interaction. <br>  </td>
      *    </tr>
      * </table>
-*/
+     */
     public okhttp3.Call getCustomerDetailCall(final ApiCallback _callback) throws ApiException {
 
         Object postBody = null;
@@ -217,7 +211,7 @@ public class CommonCustomerAPI extends ProtectedAPI {
      *        <td>  * x-v - The [version](#response-headers) of the API end point that the data holder has responded with. <br>  * x-fapi-interaction-id - An [RFC4122](https://tools.ietf.org/html/rfc4122) UUID used as a correlation id. If provided, the data holder must play back this value in the x-fapi-interaction-id response header. If not provided a [RFC4122] UUID value is required to be provided in the response header to track the interaction. <br>  </td>
      *    </tr>
      * </table>
-*/
+     */
     public ResponseCommonCustomerDetail getCustomerDetail() throws ApiException {
 
         LOGGER.trace("getCustomerDetail");
@@ -244,7 +238,7 @@ public class CommonCustomerAPI extends ProtectedAPI {
      *        <td>  * x-v - The [version](#response-headers) of the API end point that the data holder has responded with. <br>  * x-fapi-interaction-id - An [RFC4122](https://tools.ietf.org/html/rfc4122) UUID used as a correlation id. If provided, the data holder must play back this value in the x-fapi-interaction-id response header. If not provided a [RFC4122] UUID value is required to be provided in the response header to track the interaction. <br>  </td>
      *    </tr>
      * </table>
-*/
+     */
     public ApiResponse<ResponseCommonCustomerDetail> getCustomerDetailWithHttpInfo() throws ApiException {
         okhttp3.Call call = getCustomerDetailValidateBeforeCall(null);
         Type returnType = new TypeToken<ResponseCommonCustomerDetail>(){}.getType();
